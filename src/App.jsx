3e9deb72b1c826edaa54c1207a4599932c61efa2
rohil1418 <rohilk592@gmail.com>
2026-09-24 17:13:48 +0530

    feat: integrate landing page sections

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Menu from "./components/Menu";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Menu />
+      <About />
+    </>
+  );
+}
+
+export default App;
