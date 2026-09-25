30f2aaa0473b1ec40a985698c2843dad8a4a267a
rohil1418 <rohilk592@gmail.com>
2026-09-25 16:11:32 +0530

    feat: add routes for contact and booking pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,60 @@
-import './App.css'
+import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import About from "./components/About";
 import Services from "./components/Services";
-import Delivery from './components/Delivery';
-import Testimonials from './components/Testimonials';
-import Blog from './components/Blog';
-import Footer from './components/Footer';
+import Delivery from "./components/Delivery";
+import Testimonials from "./components/Testimonials";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import BookTable from "./components/BookTable";
+import MenuPage from "./components/MenuPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+
       <Navbar />
-      <Hero />
-      <Menu />
-      <About />
-      <Services/>
-      <Delivery/>
-      <Testimonials />
-      <Blog />
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <Menu />
+              <About />
+              <Services />
+              <Delivery />
+              <Testimonials />
+              <Blog />
+            </>
+          }
+        />
+
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+
+        <Route
+          path="/book-table"
+          element={<BookTable />}
+        />
+
+        <Route path="/menu" element={<MenuPage />} />
+
+      </Routes>
+
       <Footer />
-    </>
+
+    </BrowserRouter>
   );
 }
 
